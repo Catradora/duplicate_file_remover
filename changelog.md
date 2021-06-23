47713fc9cbfe2e34c6653c534e014c5bece1aa7d
Noah Hood <hood.noah@gmail.com>
2021-06-23 14:56:21 -0400

    added changelog, changed limit of number of children to print
    added ability to directly delete duplicates without copying
    
    added ability to directly delete duplicates without copying
    
    removed sample_images copy folder from tracking, updated gitignore
    
    fixed passage of invalid directories, and bug where custom duplicate directories wouldn't be created as needed

---
+++ changelog.md
@@ -0,0 +1,21 @@
+# Changelog
+
+All noteworthy changes to this project will be documented in this file.
+
+## [1.1.0] 2021-06-23
+
+### Added
+
+- functionality for choosing the folder into which duplicates should be moved, or the (default) of just deleting the duplicates
+- changelog file
+- requirements.txt for simpler portability
+
+### Fixed
+
+- changed printing of child directories to a limit of 5
+
+## [1.0.0] 2021-06-23
+
+### Added
+
+- functionality for segregating duplicate files within a parent directory into a folder titled "duplicates," distinguished from one another by a UUID
--- main.py
+++ main.py
@@ -10,10 +10,14 @@ root = tk.Tk()
 root.withdraw()
 
 
+def is_valid_directory(directory: pathlib.Path):
+    return directory.exists() and (directory != "" or directory is not None)
+
+
 def get_parent_directory():
     parent_directory = pathlib.Path(filedialog.askdirectory())
 
-    if not parent_directory.exists():
+    if not is_valid_directory(parent_directory):
         print("Path doesn't exist, try again.")
         get_parent_directory()
     else:
@@ -45,8 +49,10 @@ def create_md5_dictionary(files: List[pathlib.Path]):
     return md5_dict
 
 
-def create_duplicates_subfolder(parent_directory: pathlib.Path):
-    compound_path = parent_directory.joinpath("duplicates")
+def create_duplicates_subfolder(
+    parent_directory: pathlib.Path, dupes_subfolder: pathlib.Path
+):
+    compound_path = parent_directory.joinpath(dupes_subfolder)
 
     compound_path.mkdir(parents=False, exist_ok=True)
 
@@ -65,13 +71,44 @@ def move_duplicate_files(md5_dictionary: defaultdict, duplicates_path: pathlib.P
                 source.replace(destination)
 
 
+def delete_duplicate_files(md5_dictionary: defaultdict):
+    for _, value in md5_dictionary.items():
+        duplicates = value[1:]
+        if len(duplicates) > 0:
+            for duplicate in duplicates:
+                duplicate.unlink()
+
+
+def delete_or_cut(parent_directory: pathlib.Path):
+    delete_or_not = input(
+        "Delete files (default) or enter folder name to cut duplicates into: "
+    )
+
+    if delete_or_not == "":
+        return delete_or_not
+    else:
+        duplicates_folder = parent_directory.joinpath(delete_or_not)
+
+        try:
+            duplicates_folder.mkdir()
+            return duplicates_folder
+        except FileExistsError:
+            print("Folder already exists, input a unique file name.")
+            delete_or_not(parent_directory)
+        except:
+            print(
+                "Could not create folder. Check folder name for invalid characters, or file creation permissions."
+            )
+            delete_or_not(parent_directory)
+
+
 def main():
     parent_directory = get_parent_directory()
     children = get_children(parent_directory)
 
     print("Parent Directory:", parent_directory)
     print("Sample of children: ")
-    for child in children:
+    for child in children[:5]:
         print(child)
     correct_dir = input("Correct directory? y/n: ")
     if not correct_dir == "y":
@@ -79,9 +116,13 @@ def main():
 
     all_files = get_list_of_all_files(parent_directory)
     dupe_dict = create_md5_dictionary(all_files)
-    dupes_folder = create_duplicates_subfolder(parent_directory)
+    dupe_dir = delete_or_cut(parent_directory)
 
-    move_duplicate_files(dupe_dict, dupes_folder)
+    if dupe_dir != "":
+        dupes_folder = create_duplicates_subfolder(parent_directory, dupe_dir)
+        move_duplicate_files(dupe_dict, dupes_folder)
+    else:
+        delete_duplicate_files(dupe_dict)
 
 
 if __name__ == "__main__":
